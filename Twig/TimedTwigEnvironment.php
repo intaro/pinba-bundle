08bd6b4f56e4dd8ffe2fa823ddaf5875f7bae629
Alexey Chelnakov <ad.defas@gmail.com>
2022-07-04 18:24:14 +0300

    Remove templating.engine.twig service and use twig instead. The "templating" service is deprecated since version 4.3 and will be removed in 5.0

---
+++ Twig/TimedTwigEnvironment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Intaro\PinbaBundle\Twig;
+
+use Intaro\PinbaBundle\Stopwatch\Stopwatch;
+use Twig\Environment;
+use Twig\Loader\LoaderInterface;
+
+class TimedTwigEnvironment extends Environment
+{
+
+    private Stopwatch $stopwatch;
+
+    public function __construct(LoaderInterface $loader, $options = [], Stopwatch $stopwatch)
+    {
+        parent::__construct($loader, $options);
+
+        $this->stopwatch = $stopwatch;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render($name, array $parameters = [])
+    {
+        $e = $this->stopwatch->start(array(
+            'server' => 'localhost',
+            'group' => 'twig::render',
+            'twig_template' => (string) $name,
+        ));
+
+        $ret = parent::render($name, $parameters);
+
+        $e->stop();
+
+        return $ret;
+    }
+}
